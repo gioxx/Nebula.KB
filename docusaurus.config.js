102402c264ed2499754df09b67ac4fefc4b11443
Gioxx <giovanni@solone.it>
2025-12-17 17:09:47 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,6 +13,9 @@ const config = {
   title: 'Nebula.KB',
   tagline: 'Flawed by design, just like my code.',
   favicon: 'img/Nebula_Icon_Header.svg',
+  metadata: [
+    { name: 'algolia-site-verification', content: '2152E93E071549EB' },
+  ],
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
